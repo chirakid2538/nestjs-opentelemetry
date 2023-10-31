1aad23a647b56230ed8859998cb62976132b0caa
chirakid kaweesirikon <chirakid.k@gmail.com>
2023-10-31 16:56:12 +0700

    :bug: fixed
    fixed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async appStatus(): Promise<Record<string, any>> {
+  appStatus(): Record<string, any> {
     return this.appService.appStatus();
   }
 }
