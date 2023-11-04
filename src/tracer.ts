a0a6439a8a36dc680b1ee356aa9e126b90938050
Chirakid Kaweesirikon <aum.chirakid@gmail.com>
2023-11-04 11:01:55 +0700

    Update tracer.ts

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,26 +1,30 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { Resource } from '@opentelemetry/resources';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
+
 const options = {
-  url: String(process.env.OTEL_EXPORTER_URL),
+  url: String(process.env.OTEL_ZIPKIN_EXPORTER_URL),
   // optional interceptor
   getExportRequestHeaders: () => {
     return {};
   },
 };
-const exporter = new ZipkinExporter(options);
+
+const otelExporter = process.env.OTEL_EXPORTER ?? 'default';
+const exporter = otelExporter === 'zipkin' ? new ZipkinExporter(options) : new ConsoleSpanExporter();
+const SpanProcessor = otelExporter === 'zipkin' ? BatchSpanProcessor : SimpleSpanProcessor;
 
 export const otelSDK = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'apis',
   }),
-  spanProcessor: new BatchSpanProcessor(exporter),
+  spanProcessor: new SpanProcessor(exporter),
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
